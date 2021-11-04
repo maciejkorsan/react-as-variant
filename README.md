562d8013bf01925130748536007b53c81878bd57
maciejkorsan <maciejkorsan@gmail.com>
2021-11-04 22:20:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mj7oxa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mj7oxa)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <section>
+        <h2>Another heading</h2>
+        <p>lorem  ipsum dolor sit amet</p>
+      </section>
+    </main>
+  );
+}
